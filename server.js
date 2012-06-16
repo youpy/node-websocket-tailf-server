037febb7abe369414358dc556a14839dec438266
youpy <youpy@buycheapviagraonlinenow.com>
2012-06-17 01:27:55 +0900

    remove app

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 var
-    port            = process.env.PORT || 3000,
+    port            = process.env.PORT || 8080,
     WebSocketServer = require('ws').Server,
-    wss             = module.exports.wss = new WebSocketServer({port: 8080}),
-    express         = require('express'),
-    app             = module.exports.app = express.createServer(),
+    wss             = module.exports.wss = new WebSocketServer({port: port}),
     md5             = require('md5');
 
-app.set('view engine', 'jade');
-app.set('view options', { layout: false });
-app.use(express.static(__dirname + '/public'));
-
-app.post('/channels', function(req, res) {
+wss.on('connection/channels', function(ws) {
   var id = md5.digest_s((+ new Date()).toString());
   makeChannel(wss, id);
-  res.json({ id: id });
+  ws.send(id);
+  ws.close();
 });
-
-app.get('/channels/:id', function(req, res) {
-  var id = req.params.id;
-  res.render('channel', { id: id });
-});
-
-if(!module.parent) {
-  app.listen(port);
-}
 
 function makeChannel(wss, path) {
-  wss.on('connection/' + path, function(ws) {
+  wss.on('connection/channels/' + path, function(ws) {
     ws.path = path;
     ws.on('message', function(message) {
       wss.clients.filter(function(client) {
